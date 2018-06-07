d6fd6486b837e66020d716f79042fe61003b7e4d
Stevan Stojicevic <stevanstojicevic95@gmail.com>
2018-06-07 15:09:26 +0200

    Returning back.

---
+++ battle_city_design/SDK/SDK_Workspace/super_mario/src/map.h
@@ -0,0 +1,10 @@
+#ifndef _MAP_H_
+#define _MAP_H_
+
+unsigned char  map1[30][160];
+unsigned char map[30][40];
+unsigned char lose_screen[30][40];
+unsigned char map_win[30][160];
+
+
+#endif // _MAP_H_
